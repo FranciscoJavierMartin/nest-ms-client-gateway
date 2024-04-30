9c9f5ab840e408a75a5f75d203944ff3047f32df
Francisco <javiermartincuenca@gmail.com>
2024-04-30 20:52:42 +0200

    Change order status

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -7,12 +7,14 @@ import {
   Inject,
   ParseUUIDPipe,
   Query,
+  Patch,
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { ORDER_SERVICE } from '../config';
 import { catchError } from 'rxjs';
 import { OrdersPaginationDto } from '../common/dto/orders-pagination.dto';
+import { StatusDto } from './dto/status.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -37,5 +39,19 @@ export class OrdersController {
         throw new RpcException(err);
       }),
     );
+  }
+
+  @Patch(':id')
+  changeOrderStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: StatusDto,
+  ) {
+    return this.ordersClient
+      .send('changeOrderStatus', { id, ...statusDto })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
   }
 }
